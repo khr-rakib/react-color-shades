4687ce8b41ab1170b9501b2b7b4f459e602d6b0f
khr-rakib <khr.rakib@gmail.com>
2020-08-21 19:32:28 +0600

    Updates

---
+++ precache-manifest.2c191cbda3121bf4f2cde677011ba4c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b97cb617c7d8860f4cf4b1e6155908f",
+    "url": "/react-color-shades/index.html"
+  },
+  {
+    "revision": "f518406840b9e962ebad",
+    "url": "/react-color-shades/static/css/main.df23c846.chunk.css"
+  },
+  {
+    "revision": "bd8aef79120121c021e4",
+    "url": "/react-color-shades/static/js/2.ecec42cf.chunk.js"
+  },
+  {
+    "revision": "62afbf7b6a84202ba7762582324f8a1a",
+    "url": "/react-color-shades/static/js/2.ecec42cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f518406840b9e962ebad",
+    "url": "/react-color-shades/static/js/main.032b96ed.chunk.js"
+  },
+  {
+    "revision": "4f0ea9200a511496b79c",
+    "url": "/react-color-shades/static/js/runtime-main.5e631b42.js"
+  },
+  {
+    "revision": "f12785bebe28f7ea8fa9aa0ced9c4d85",
+    "url": "/react-color-shades/static/media/audio.f12785be.mp3"
+  }
+]);
